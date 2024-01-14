8739fcdd0a13edbcf954e15140e61250e2f6c078
GopaallYamala <Gopal.yamala@outlook.com>
2024-01-14 19:24:52 +0530

    schema

---
+++ src/api/resource-article/controllers/resource-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resource-article controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::resource-article.resource-article');
---
+++ src/api/resource-article/routes/resource-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resource-article router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::resource-article.resource-article');
